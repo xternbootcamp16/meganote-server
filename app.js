f4742ea2cd21f7913a832d26a723d153095dcc51
Dave Strus <dave@getfretless.com>
2016-06-16 15:34:18 -0400

    Extract Note model and schema to a module.

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 require('dotenv').load();
 var express = require('express');
-var db = require('./config/db');
+var Note = require('./models/note');
 
 var app = express();
-
-var noteSchema = db.Schema({
-  title: String,
-  body_html: String,
-  body_text: String,
-  updated_at: { type: Date, default: Date.now }
-});
-var Note = db.model('Note', noteSchema);
 
 app.get('/', function(req, res) {
   Note
