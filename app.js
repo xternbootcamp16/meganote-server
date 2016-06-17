68b8ba2f3a0b631752f493e2f2d71013454418dc
Dave Strus <dave@getfretless.com>
2016-06-17 19:12:45 -0400

    Send an error message when updating fails.

--- app.js
+++ app.js
@@ -67,18 +67,25 @@ app.put('/:id', function(req, res) {
     .findOne({
       _id: req.params.id
     })
-    .then(function(note) {
-      note.title = req.body.note.title;
-      note.body_html = req.body.note.body_html;
-      note
-        .save()
-        .then(function() {
-          res.json({
-            message: 'Your changes have been saved.',
-            note: note
+    .then(
+      function(note) {
+        note.title = req.body.note.title;
+        note.body_html = req.body.note.body_html;
+        note
+          .save()
+          .then(function() {
+            res.json({
+              message: 'Your changes have been saved.',
+              note: note
+            },
+            function(result) {
+              res.json({ message: 'Aww, cuss!' });
+            });
           });
-        });
-    });
+      },
+      function(result) {
+        res.json({ message: 'Aww, cuss!' });
+      });
 });
 
 app.delete('/:id', function(req, res) {
