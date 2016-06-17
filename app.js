a6fef0bb4c631311edf85ccb7f7a9df867cbde29
Dave Strus <dave@getfretless.com>
2016-06-17 13:35:42 -0400

    Create notes from POST.

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 require('dotenv').load();
 var express = require('express');
 var Note = require('./models/note');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   Note
     .find()
     .then(function(notes) {
       res.json(notes);
+    });
+});
+
+app.post('/', function(req, res) {
+  var note = new Note({
+    title: req.body.note.title,
+    body_html: req.body.note.body_html
+  });
+
+  note
+    .save()
+    .then(function(noteData) {
+      res.json({
+        message: 'Successfully created note',
+        note: noteData
+      });
     });
 });
 
