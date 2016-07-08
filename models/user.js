9626340c46abdce3a1de0ba8437ce7b21b3672b8
Dave Strus <dave@getfretless.com>
2016-07-08 09:54:17 -0400

    Add User model.

---
+++ models/user.js
@@ -0,0 +1,26 @@
+var db = require('../config/db');
+
+var userSchema = db.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+userSchema.pre('save', function(next) {
+  this.updated_at = Date.now();
+  next();
+});
+
+var User = db.model('User', userSchema);
+
+module.exports = User;
