12d6aeb9d80b1e198233f3cbde19bcbd75a95ca0
Dave Strus <dave@getfretless.com>
2016-07-08 10:19:06 -0400

    Verify that password and passwordConfirmation match.

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -3,6 +3,13 @@ var User = require('../models/user');
 
 // CREATE a user
 router.post('/', function(req, res) {
+  if (!passwordsPresent(req.body.user) || !passwordsMatch(req.body.user)) {
+    res.status(422).json({
+      message: 'Passwords must match!'
+    });
+    return;
+  }
+
   var user = new User({
     name: req.body.user.name,
     username: req.body.user.username,
@@ -16,3 +23,11 @@ router.post('/', function(req, res) {
 });
 
 module.exports = router;
+
+function passwordsPresent(payload) {
+  return (payload.password && payload.passwordConfirmation)
+}
+
+function passwordsMatch(payload) {
+  return (payload.password === payload.passwordConfirmation)
+}
