55b615addfee4e4d20ff06c25acd431135f5c2fc
Dave Strus <dave@getfretless.com>
2016-07-08 09:25:48 -0400

    Mount note routes at a namespaced path.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').load();
 var express = require('express');
-var Note = require('./models/note');
 var bodyParser = require('body-parser');
+var noteRoutes = require('./routes/note-routes');
 
 var app = express();
 
@@ -23,89 +23,8 @@ app.use(function(req, res, next) {
 // Body parsing for JSON POST/PUT payloads
 app.use(bodyParser.json());
 
-// READ all notes
-app.get('/', function(req, res) {
-  Note
-    .find()
-    .sort({ updated_at: 'desc' })
-    .then(function(notes) {
-      res.json(notes);
-    });
-});
-
-// READ one note
-app.get('/:id', function(req, res) {
-  Note
-    .findOne({
-      _id: req.params.id
-    })
-    .then(function(note) {
-      res.json(note);
-    });
-});
-
-// CREATE a note
-app.post('/', function(req, res) {
-  var note = new Note({
-    title: req.body.note.title,
-    body_html: req.body.note.body_html
-  });
-
-  note
-    .save()
-    .then(function(noteData) {
-      res.json({
-        message: 'Successfully created note',
-        note: noteData
-      });
-    });
-});
-
-// UPDATE a note
-app.put('/:id', function(req, res) {
-  Note
-    .findOne({
-      _id: req.params.id
-    })
-    .then(
-      function(note) {
-        note.title = req.body.note.title;
-        note.body_html = req.body.note.body_html;
-        note
-          .save()
-          .then(
-            function() {
-              res.json({
-                message: 'Your changes have been saved.',
-                note: note
-              });
-            },
-            function(result) {
-              res.json({ message: 'Aww, cuss!' });
-            }
-          );
-      },
-      function(result) {
-        res.json({ message: 'Aww, cuss!' });
-      });
-});
-
-app.delete('/:id', function(req, res) {
-  Note
-    .findOne({
-      _id: req.params.id
-    })
-    .then(function(note) {
-      note
-        .remove()
-        .then(function() {
-          res.json({
-            message: 'That note has been deleted.',
-            note: note
-          })
-        });
-    });
-});
+// Routes
+app.use('/api/v1/notes', noteRoutes);
 
 // CREATE a user
 app.post('/users', function(req, res) {
