a1b553988a6bbd6e2e6b9194b4f8ebbf40829798
Dave Strus <dave@getfretless.com>
2016-07-08 10:02:54 -0400

    Create users, ignoring passwords.

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,10 +1,18 @@
 var router = require('express').Router();
+var User = require('../models/user');
 
 // CREATE a user
 router.post('/', function(req, res) {
-  res.json({
-    msg: 'HOORAY!'
+  var user = new User({
+    name: req.body.user.name,
+    username: req.body.user.username,
   });
+
+  user
+    .save()
+    .then(
+      userData => res.json(userData)
+    );
 });
 
 module.exports = router;
