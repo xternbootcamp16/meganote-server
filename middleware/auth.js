272530344fc593fcb4edff059160971c04a5425b
Dave Strus <dave@getfretless.com>
2016-07-12 15:48:38 -0400

    Remove unnecessary check for token presence.

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,36 +6,30 @@ module.exports = (req, res, next) => {
 
   if (isPreflight(req) || isLoggingInOrSigningUp(req)) { next(); return; }
 
-  if (token) {
-    // verify the token
-    jwt.verify(token, process.env.JWT_SECRET, (err, decodedPayload) => {
-      if (err) {
-        // invalid token
-        res.status(401).json({ message: 'Authentication required.' });
-        return;
-      }
+  jwt.verify(token, process.env.JWT_SECRET, (err, decodedPayload) => {
+    if (err) {
+      // invalid token
+      res.status(401).json({ message: 'Authentication required.' });
+      return;
+    }
 
-      // find the user
-      User
-        .findOne({ _id: decodedPayload._id })
-        .then(
-          user => {
-            if (user) {
-              // add the user to the request
-              req.user = user;
-              next();
-            }
-            else {
-              // user not found
-              res.status(401).json({ message: 'Authentication required.' });
-            }
+    // find the user
+    User
+      .findOne({ _id: decodedPayload._id })
+      .then(
+        user => {
+          if (user) {
+            // add the user to the request
+            req.user = user;
+            next();
           }
-        );
-    });
-  }
-  else {
-    res.status(401).json({ message: 'Authentication required.' });
-  }
+          else {
+            // user not found
+            res.status(401).json({ message: 'Authentication required.' });
+          }
+        }
+      );
+  });
 }
 
 function isPreflight(req) {
