947c16b15a9382181af56a2634ec6e580de922f0
Dave Strus <dave@getfretless.com>
2016-07-12 10:40:49 -0400

    Verify JWT and add user to request.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,37 @@
+var jwt = require('jsonwebtoken');
+var User = require('../models/user');
+
 module.exports = (req, res, next) => {
   if (isPreflight(req) || isLoggingInOrSigningUp(req)) { next(); return; }
   const token = req.headers.authorization;
   if (token) {
+
     // verify the token
-    // get the user
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedPayload) => {
+      
+      if (!decodedPayload) {
+        // token verification failed
+        res.status(401).json({ message: 'Authentication required.' });
+        return;
+      }
+
+      // find the user
+      User
+        .findOne({ _id: decodedPayload._id })
+        .then(
+          user => {
+            if (user) {
+              // add the user to the request
+              req.user = user;
+              next();
+            }
+            else {
+              // user not found
+              res.status(401).json({ message: 'Authentication required.' });
+            }
+          }
+        );
+    })
     next();
   }
   else {
