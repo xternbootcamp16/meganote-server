2c5018fe302d15d71b2079b34eaf2b2be4754877
Dave Strus <dave@getfretless.com>
2016-09-09 16:22:38 -0400

    Break it.

--- app.js
+++ app.js
@@ -2,27 +2,30 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').load();
 }
 var express = require('express');
-var bodyParser = require('body-parser');
-var noteRoutes = require('./routes/note-routes');
-var userRoutes = require('./routes/user-routes');
-var sessionRoutes = require('./routes/session-routes');
-var headersMiddleware = require('./middleware/headers');
-var authMiddleware = require('./middleware/auth');
+// var bodyParser = require('body-parser');
+// var noteRoutes = require('./routes/note-routes');
+// var userRoutes = require('./routes/user-routes');
+// var sessionRoutes = require('./routes/session-routes');
+// var headersMiddleware = require('./middleware/headers');
+// var authMiddleware = require('./middleware/auth');
 
 var app = express();
 app.set('port', (process.env.PORT || 3030));
+router.get('/', function(req, res) {
+  //
+});
 
 // Middleware
-app.use(headersMiddleware);
-app.use(authMiddleware);
+// app.use(headersMiddleware);
+// app.use(authMiddleware);
 
 // Body parsing for JSON POST/PUT payloads
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 // Routes
-app.use('/api/v1/notes', noteRoutes);
-app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/sessions', sessionRoutes);
+// app.use('/api/v1/notes', noteRoutes);
+// app.use('/api/v1/users', userRoutes);
+// app.use('/api/v1/sessions', sessionRoutes);
 
 
 app.listen(app.get('port'), function() {
